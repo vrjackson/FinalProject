be6674351a168a95d73829b029ea7fd99ed1b550
Emily <ehalferty72@gmail.com>
2025-04-01 13:39:33 -0400

    Added a scanner to main

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is the Final Project Repository.");
         System.out.println("This is an additional line of code!");
+
+        Scanner in = new Scanner(System.in);
 
     }
 }
